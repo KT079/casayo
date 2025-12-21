dd675fde2690fb27e8d6dabd15a6738a21c6ada0
KT079 <kushtado2@gmail.com>
2025-12-21 14:41:19 +0530

    Delete Casayo-READY-DEPLOY directory

--- Casayo-READY-DEPLOY/app/layout.tsx
+++
@@ -1,8 +0,0 @@
-import "./globals.css";
-export default function RootLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <html lang="en">
-      <body className="bg-zinc-950 text-white">{children}</body>
-    </html>
-  );
-}
